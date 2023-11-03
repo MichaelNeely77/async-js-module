2d9a518f07c1f6797717775e501122c464cc5415
Michael Neely <michael@michaelneely.net>
2023-11-03 10:38:45 -0400

    Cleaning up tree

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const output = document.querySelector('p');
 function trackUserHandler() {
   navigator.geolocation.getCurrentPosition(
 posData => {
+  setTimeout(() => {
     console.log(posData);
+    }, 2000)
   },
       error => {
     console.log(error);
