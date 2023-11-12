ad5902685c89281d11e5979e4898404457021b57
Michael Neely <michael@michaelneely.net>
2023-11-12 12:05:25 -0500

    Catching up tree

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ error => {
   return promise;
 };
 
-const setTimer = (duration) => {
+const setTimer = duration => {
   const promise = new Promise((resolve, reject) => {
     setTimeout(() => {
       resolve("Done!");
@@ -24,23 +24,26 @@ const setTimer = (duration) => {
   return promise;
 };
 
-function trackUserHandler() {
-  let positionData;
-  getPosition()
-      .then(posData => {
-    positionData = posData;
-    return setTimer(2000);
-    })
-      .catch(err => {
-        console.log(err);
-      })
-      .then(data => {
-        console.log(data, positionData);
-      });
-  setTimer(1000).then(() => {
-    console.log("Timer done...");
-  });
-  console.log("Getting Tracking...");
+async function trackUserHandler() {
+  // let positionData;
+  const posData = await getPosition();
+  const timerData = await setTimer(2000);
+  console.log(timerData, posData);
+      // .then(posData => {
+      // positionData = posData;
+      // return setTimer(2000);
+    // })
+    //   .catch(err => {
+    //     console.log(err);
+    //     return 'on we go...';
+    //   })
+      // .then(data => {
+      //   console.log(data, positionData);
+      // });
+  // setTimer(1000).then(() => {
+  //   console.log("Timer done...");
+  // });
+  // console.log("Getting Tracking...");
 }
 
 
